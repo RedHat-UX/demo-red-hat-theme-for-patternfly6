24ffe5da2f92bc9127cc75311ef73d6e842a2452
ArathyKumar <ask6295@gmail.com>
2025-03-27 19:41:03 +0530

    chore: created a CSS file for the workshop

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import PfForm from "./components/Form/Form";
 import PfAccordion from "./components/Accordion/Accordion";
 import PfCard from "./components/Card/Card";
 //import 'redhat-brand-theme/dist/redhat-brand-theme.css';
-import './pf-workshop.css';
-
+import './pf-workshop.css'; // Will add the CSS here as part of the workshop
+ 
 function App() {
 
   return (
