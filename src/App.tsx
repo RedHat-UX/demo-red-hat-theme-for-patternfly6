f2ffb1c32a9c94ad2f1b587230b1f4fac39e782b
ArathyKumar <ask6295@gmail.com>
2025-04-09 13:38:25 +0530

    chore: table style fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ import PfForm from "./components/Form/Form";
 import PfAccordion from "./components/Accordion/Accordion";
 import PfCard from "./components/Card/Card";
 import PfTable from "./components/Table/Table";
-import 'redhat-brand-theme/dist/redhat-brand-theme.css';
+// import 'redhat-brand-theme/dist/redhat-brand-theme.css';
+import './rhds-theme.css';
 
 function App() {
 
